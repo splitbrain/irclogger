caea4493625de5a32d775a4b5cc3197dfde4f082
Andrwe Lord Weber <github@andrwe.org>
2013-08-04 12:40:48 +0200

    change config file
    Signed-off-by: Andrwe Lord Weber <github@andrwe.org>

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL & ~E_NOTICE);
 
 require("func.php");
-$conf = loadconfig('irclogger.config.php');
+$conf = loadconfig('irclog.config.php');
 
 
 header('Content-Type: text/html; charset=utf-8');
